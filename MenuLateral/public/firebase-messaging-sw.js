25a90f927d8c0749e6e061dbc09f253be9ec5e8d
Dann-VJ <danielvazquezjoaquin236@gmail.com>
2024-04-09 10:08:12 -0600

    pushnotifications ok

---
+++ MenuLateral/public/firebase-messaging-sw.js
@@ -0,0 +1,26 @@
+﻿importScripts("https://www.gstatic.com/firebasejs/10.9.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.9.0/firebase-messaging-compat.js");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB7TCK4XB22qI2HPLCQi4PMA5h3oNS4WAA",
+  authDomain: "pwa-service-998cd.firebaseapp.com",
+  projectId: "pwa-service-998cd",
+  storageBucket: "pwa-service-998cd.appspot.com",
+  messagingSenderId: "128611623155",
+  appId: "1:128611623155:web:27cf1a8e901389a769d786",
+  measurementId: "G-TXZWJZ0LYJ"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging(app);
+
+messaging.onBackgroundMessage(payload => {
+    console.log('Recibiendo mensajes en segundo plano');
+    const tituloNotificacion = payload.notification.title;
+    const options = {
+        body: payload.notification.body,
+        // icon: '../img/40.png',
+    };
+    self.registration.showNotification(tituloNotificacion, options);
+})
---
+++ MenuLateral/src/firebase.js
@@ -0,0 +1,18 @@
+﻿import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getMessaging, getToken } from "firebase/messaging";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB7TCK4XB22qI2HPLCQi4PMA5h3oNS4WAA",
+  authDomain: "pwa-service-998cd.firebaseapp.com",
+  projectId: "pwa-service-998cd",
+  storageBucket: "pwa-service-998cd.appspot.com",
+  messagingSenderId: "128611623155",
+  appId: "1:128611623155:web:27cf1a8e901389a769d786",
+  measurementId: "G-TXZWJZ0LYJ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const messaging = getMessaging(app);
